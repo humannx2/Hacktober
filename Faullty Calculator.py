79ee861210cefda942d5c97b3419f0d48f1ef815
humannx2 <116998737+humannx2@users.noreply.github.com>
2022-10-31 23:18:39 +0530

    Add files via upload

---
+++ Faullty Calculator.py
@@ -0,0 +1,21 @@
+#Design a calculator which will give all the correct values for calculations except
+# 55*8=333, 22/3=12
+'''Take operator and operand from user input'''
+print("Enter a and b")
+a=int(input())
+b=int(input())
+print("Enter the operator")
+op=input()
+if (a==55 and b==8 and op=='*'):
+    print(333)
+elif(a==22 and b==3 and op=='/'):
+    print(12)
+else:
+    if op=='+':
+        print(a+b)
+    elif op=='-':
+        print(a-b)
+    elif op=='*':
+        print(a*b)
+    else:
+        print(a/b)
