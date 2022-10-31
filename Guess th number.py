b315a3d46355bd18a06f2134b77e18ee1840e7a4
humannx2 <116998737+humannx2@users.noreply.github.com>
2022-10-31 23:32:53 +0530

    Add files via upload

---
+++ Guess th number.py
@@ -0,0 +1,10 @@
+n=18
+a=int(input("enter the number\n"))
+if (a > n):
+    print("Number entered is bigger")
+else:
+    if (a==n):
+        print("Number guessed is right")
+    else :
+        print("Number entered is smaller")
+
